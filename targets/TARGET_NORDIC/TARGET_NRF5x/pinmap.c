71b0445e5d37f62ebcd2a98659d4abf9ee9d4e09
Blackstone Engineering <austin.blackstone@arm.com>
2018-02-14 07:53:36 -0600

    moved pre-defined peripheral pinmaps to Peripheralnames.c

--- targets/TARGET_NORDIC/TARGET_NRF5x/pinmap.c
+++ targets/TARGET_NORDIC/TARGET_NRF5x/pinmap.c
@@ -43,23 +43,6 @@ void pin_mode(PinName pin, PinMode mode)
 
 // ------------------ Multiplexed Additions -------------------//
 
-// TODO: move this to PeripheralPins.c
-// Optional: Add Peripherals here
-/*
-/************SPI***************
-const MultiPinMap SPI_Pinmap[]= {
-	{SPI1_MOSI, SPI1_MISO, SPI1_SS, SPI1_SCLK, 0},// SPI1 on Hardware Instance 0
-	{SPI2_MOSI, SPI2_MISO, SPI2_SS, SPI2_SCLK, 1}
-};
-
-/************I2C***************
-const MultiPinMap I2C_Pinmap[]={
-	{I2C1_SDA, I2C1_SDL, 1}, // I2C1 on Hardware Instance 1
-	{I2C2_SDA, I2C2_SDL, 4}  // I2C2 on Hardware Instance 4
-
-};
-*/
-
 // Hardare instances, #defined. Move to PinNames.h ?
 // Define as number of instances. Instances start at 0, but count starts at 1. So if there are 3 instances there will be instance0-instance2, but the #define below will be 3. 
 // TODO: fill in with correct defines
