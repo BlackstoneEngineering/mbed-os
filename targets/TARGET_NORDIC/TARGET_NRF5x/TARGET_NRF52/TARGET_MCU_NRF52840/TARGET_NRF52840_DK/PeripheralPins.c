352f45d189cb91b707e360644768d700d988ed36
Blackstone Engineering <austin.blackstone@arm.com>
2018-02-14 09:01:07 -0600

    moved PeripheralNames.c -> PeripheralPins.c

---
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/TARGET_MCU_NRF52840/TARGET_NRF52840_DK/PeripheralPins.c
@@ -0,0 +1,34 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "PeripheralPins.h"
+
+// TODO: move this to PeripheralPins.c
+// Optional: Add Peripherals here
+/*
+//************SPI***************
+const MultiPinMap SPI_Pinmap[]= {
+	{SPI1_MOSI, SPI1_MISO, SPI1_SS, SPI1_SCLK, 0},// SPI1 on Hardware Instance 0
+	{SPI2_MOSI, SPI2_MISO, SPI2_SS, SPI2_SCLK, 1}
+};
+
+//************I2C***************
+const MultiPinMap I2C_Pinmap[]={
+	{I2C1_SDA, I2C1_SDL, 1}, // I2C1 on Hardware Instance 1
+	{I2C2_SDA, I2C2_SDL, 4}  // I2C2 on Hardware Instance 4
+
+};
+/*/
